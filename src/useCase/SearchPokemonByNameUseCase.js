796af130a3ea02f8a4f380a06847f05bec1e7580
Giordano94 <giordano.mendes94@gmail.com>
2024-07-17 17:57:17 -0300

    refactor(useCase): refactor SearchPokemonByNameUseCase for improved Pokemon search

--- src/useCase/SearchPokemonByNameUseCase.js
+++ src/useCase/SearchPokemonByNameUseCase.js
@@ -1,4 +1,5 @@
 import Fuse from 'fuse.js';
+import { pokemonAdapter } from '../config/adapters/pokemonAdapterDto.js';
 
 export class SearchPokemonByNameUseCase {
     constructor(pokeApiGateway) {
@@ -7,20 +8,14 @@ export class SearchPokemonByNameUseCase {
 
     async execute(name) {
         try {
-            console.log('SearchPokemonByNameUseCase');
-            console.log('name', name);
             const getFullPokemonList = await this.pokeApiGateway.getPokemonList(150, 0);
             const pokemonNames = getFullPokemonList.results.map(pokemon => pokemon.name.toLowerCase());
-            console.log('pokemonNames', pokemonNames)
             const fuse = new Fuse(pokemonNames, {
                 threshold: 0.3,
                 findAllMatches: true,
                 shouldSort: true
             })
-            console.log('fuse')
-            // fuzzy search pokemon name
             const searchResult = fuse.search((name.toLowerCase()));
-            console.log('searchResult', searchResult)
             if (searchResult.length === 0) {
                 return { error: 'Pokemon not found' };
             }
@@ -31,24 +26,7 @@ export class SearchPokemonByNameUseCase {
                 if (!pokemon) return { error: 'Pokemon not found' };
                 pokemonSet.push(pokemon);
             }
-            // console.log('pokemonSet', pokemonSet)
-            return pokemonSet.map((pokemonItem) => {
-                return {
-                    name: pokemonItem.name,
-                    id: pokemonItem.id,
-                    height: pokemonItem.height,
-                    weight: pokemonItem.weight,
-                    abilities: pokemonItem.abilities.map(ability => ability.ability.name),
-                    types: pokemonItem.types.map(type => type.type.name),
-                    stats: pokemonItem.stats.map(stat => {
-                        return {
-                            name: stat.stat.name,
-                            base_stat: stat.base_stat
-                        }
-                    }),
-                    image: pokemonItem.sprites.front_default
-                }
-            });
+            return pokemonAdapter(pokemonSet);
 
         } catch (error) {
             return { error: error.message };
