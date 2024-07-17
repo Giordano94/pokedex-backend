ab5f67df3adeea075b5d36900ccc0825fa3666b5
Giordano94 <giordano.mendes94@gmail.com>
2024-07-17 15:20:03 -0300

    feat(server): add CORS support to express server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import Express from 'express';
-import { hostname, port } from './config/infra/environment.js'
-import { setupMiddlewareRoutes } from './config/infra/middleware/setupRoutes.js'
+import { hostname, port } from './config/infra/environment.js';
+import { setupMiddlewareRoutes } from './config/infra/middleware/setupRoutes.js';
+import cors from 'cors';
 
 (async () => {
-    const server = Express();
+  const server = Express();
+  server.use(cors('*'));
+  setupMiddlewareRoutes(server);
 
-    setupMiddlewareRoutes(server);
-
-    server.listen(port, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+  server.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 })();
