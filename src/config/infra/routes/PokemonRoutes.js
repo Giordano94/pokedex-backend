cc1d7809f2fa7878d266bf9dca9f9e4ad3dbdb38
Giordano94 <giordano.mendes94@gmail.com>
2024-07-17 21:52:00 -0300

    feat(PokemonRoutes): add pokemonFavoriteRoutes

--- src/config/infra/routes/PokemonRoutes.js
+++ src/config/infra/routes/PokemonRoutes.js
@@ -1,20 +1,29 @@
 import { Router } from 'express';
-import { PokemonListController } from '../../../controller/Pokemon/PokemonListController.js';
-import { PokemonGetCharacteristicsController } from '../../../controller/Pokemon/PokemonGetCharacteristics.js';
-import { PokemonSearchByNameController } from '../../../controller/Pokemon/PokemonSearchByNameController.js';
-import { PokemonSearchByIdController } from '../../../controller/Pokemon/PokemonSearchByIdController.js';
+import { PokemonFavoriteListController } from '../../../controller/Pokemon/PokemonFavoriteListController.js';
+import { PokemonRepositoryPrisma } from '../repository/PokemonRepositoryPrisma.js';
 import { PokeApiGateway } from '../gateway/pokeApiGateway.js';
+import { PokemonFavoriteByIdController } from '../../../controller/Pokemon/PokemonFavoriteByIdController.js';
+import { PokemonUnfavoriteByIdController } from '../../../controller/Pokemon/PokemonUnfavoriteByIdController.js';
 
 const pokemonRoutes = Router();
+const pokemonRepository = new PokemonRepositoryPrisma();
+const pokeApiGateway = new PokeApiGateway();
 
-const pokemonListController = new PokemonListController(PokeApiGateway);
-const pokemonSearchByNameController = new PokemonSearchByNameController(PokeApiGateway);
-const pokemonSearchByIdController = new PokemonSearchByIdController(PokeApiGateway);
-const pokemonGetCharacteristicsController = new PokemonGetCharacteristicsController(PokeApiGateway);
+console.log('controller', pokemonRepository, pokeApiGateway);
 
-pokemonRoutes.get('/list', pokemonListController.handle);
-pokemonRoutes.get('/characteristics/:id', pokemonGetCharacteristicsController.handle);
-pokemonRoutes.get('/searchByName/:name', pokemonSearchByNameController.handle);
-pokemonRoutes.get('/searchById/:pokemonId', pokemonSearchByIdController.handle);
+const pokemonFavoriteListController = new PokemonFavoriteListController(
+  pokemonRepository,
+  PokeApiGateway
+);
+const pokemonFavoriteById = new PokemonFavoriteByIdController(
+  pokemonRepository
+);
+const pokemonUnFavoriteById = new PokemonUnfavoriteByIdController(
+  pokemonRepository
+);
+
+pokemonRoutes.get('/favorite/list', pokemonFavoriteListController.handle);
+pokemonRoutes.patch('/favorite/:id', pokemonFavoriteById.handle);
+pokemonRoutes.patch('/favorite/remove/:id', pokemonUnFavoriteById.handle);
 
 export { pokemonRoutes };
