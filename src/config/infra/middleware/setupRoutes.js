502c034a9d7db138497326f284589ae5c044fc71
Giordano94 <giordano.mendes94@gmail.com>
2024-07-17 21:46:26 -0300

    refactor(setupRoutes): reorganize middleware routes setup

--- src/config/infra/middleware/setupRoutes.js
+++ src/config/infra/middleware/setupRoutes.js
@@ -1,10 +1,7 @@
+import { pokeApiRoutes } from '../routes/PokeApiRoutes.js';
 import { pokemonRoutes } from '../routes/PokemonRoutes.js';
 
 export function setupMiddlewareRoutes(server) {
-    server.use('/pokeapi', pokemonRoutes);
-    // server.use('/favorite', pokemonRoutes);
-
-    // server.use('/characteristic', pokemonRoutes);
-    // server.use('/pokemon', pokemonRoutes);
-    // server.use('/pokemon', pokemonRoutes);
+    server.use('/pokeapi', pokeApiRoutes);
+    server.use('/pokemon', pokemonRoutes);
 }
