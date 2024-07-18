5ddc57ad54dc90cbd533878a98001aae1a2145b4
Giordano94 <giordano.mendes94@gmail.com>
2024-07-17 22:10:20 -0300

    feat(controller): refactor PokemonListController to use ListDetailedPokemonUseCase

--- src/controller/Pokemon/PokemonListController.js
+++ src/controller/Pokemon/PokemonListController.js
@@ -1,17 +1,28 @@
-export class PokemonListController {
-    constructor(pokeApiGateway) {
-        this.pokeApiGateway = pokeApiGateway;
-        this.handle = this.handle.bind(this);
-    }
+import { ListDetailedPokemonUseCase } from '../../useCase/ListDetailedPokemonUseCase.js';
 
-    async handle(req, res) {
-        try {
-            let limit = req.query.limit || 150;
-            let offset = req.query.offset || 0;
-            const pokemonList = await this.pokeApiGateway.getPokemonList(limit, offset);
-            return res.status(200).json(pokemonList);
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
-        }
+export class PokemonListController {
+  constructor(pokeApiGateway) {
+    this.pokeApiGateway = pokeApiGateway;
+    this.handle = this.handle.bind(this);
+  }
+
+  async handle(req, res) {
+    try {
+      let limit = req.query.limit || 150;
+      let offset = req.query.offset || 0;
+      const listDetailedPokemonUseCase = new ListDetailedPokemonUseCase(
+        this.pokeApiGateway
+      );
+      const pokemonList = await this.pokeApiGateway.getPokemonList(
+        limit,
+        offset
+      );
+      const response = await listDetailedPokemonUseCase.execute(
+        pokemonList.results
+      );
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
+  }
 }
